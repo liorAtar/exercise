b054c034726c1785668773830dc04738ae2efd48
lior atar <lioratar@h-MacBook-Pro-sl-lior-2.local>
2021-10-17 20:47:12 +0300

    Updated the counter of active todo's

--- js/controller.js
+++ js/controller.js
@@ -52,7 +52,7 @@ export default class Controller {
 			this.view.clearNewTodo();
 			this._filter(true);
 		});
-		
+
 	}
 
 	editItemSave(id, title) {
@@ -111,7 +111,7 @@ export default class Controller {
 		}
 
 		this.store.count((total, active, completed) => {
-			this.view.setItemsLeft(0);
+			this.view.setItemsLeft(active);
 			this.view.setClearCompletedButtonVisibility(completed);
 
 			this.view.setCompleteAllCheckbox(completed === total);
